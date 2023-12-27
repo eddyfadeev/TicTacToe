b60177154a64f4b8d1332b98f8a12f5a8bffec53
Eduard Fadieiev <eddy.fadeev@gmail.com>
2023-12-27 03:07:00 -0700

    Initial commit

--- Field.cs
+++ Field.cs
@@ -1,6 +1,40 @@
 ﻿namespace TicTacToe;
 
+// TODO: Implement this class.
 public class Field
 {
+    private readonly char[,] _fieldArray;
     
+    public Field()
+    {
+        _fieldArray = new char[3, 3];
+        for (int i = 0; i < _fieldArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < _fieldArray.GetLength(1); j++)
+            {
+                _fieldArray[i, j] = ' ';
+            }
+        }
+    }
+
+    // TODO: Implement this method.
+    public void DrawField()
+    {
+        throw new NotImplementedException();
+        Console.WriteLine("-----+-----+-----");
+        for (int i = 0; i < _fieldArray.GetLength(0); i++)
+        {
+            Console.Write(" ");
+            for (int j = 0; j < _fieldArray.GetLength(1); j++)
+            {
+                Console.WriteLine($"  {_fieldArray[i, j]}  |  {_fieldArray[0, 1]}  |  {_fieldArray[0, 2]}  ");
+                
+            }
+        }
+        Console.WriteLine("-----+-----+-----");
+        Console.WriteLine($"  {_fieldArray[1, 0]}  |  {_fieldArray[1, 1]}  |  {_fieldArray[1, 2]}  ");
+        Console.WriteLine("-----+-----+-----");
+        Console.WriteLine($"  {_fieldArray[2, 0]}  |  {_fieldArray[2, 1]}  |  {_fieldArray[2, 2]}  ");
+        Console.WriteLine("-----+-----+-----");
+    }
 }
--- tools/GameUtils.cs
+++ tools/GameUtils.cs
@@ -1,6 +1,32 @@
 ﻿namespace TicTacToe;
 
-public class GameUtils
+static class GameUtils
 {
-    
-}
+    /**
+    * <summary>
+    * Gets int input from the user, check if it is within the range.
+    * Prints error message if input is invalid.
+    * </summary>
+    * <param name="lowerBond">Lower bond of valid options in form of int.</param>
+    * <param name="upperBond">Upper bond of valid options in form of int.</param>
+    * <returns>Valid input from the user as an int.</returns>
+    */
+    internal static int GetInput(int lowerBond, int upperBond)
+    {
+        // Loop until valid input is received.
+        while (true)
+        {
+            // Get input from the user, trim it and convert to uppercase.
+            string input = Console.ReadLine().Trim().ToUpper();
+            // Try to convert input to char.
+            bool parsed = int.TryParse(input, out int convertedInput);
+            // Check if input is within the range.
+            bool withinRange = convertedInput >= lowerBond && convertedInput <= upperBond;
+            // Check if input is valid and return value.
+            if (parsed && withinRange) return convertedInput; 
+            
+            // Print error message.
+            Console.WriteLine($"Invalid input. Please enter between {lowerBond} and {upperBond}.");
+        } // end of while loop.
+    } // end of GetInput(int) method.
+} // end of GameUtils class.
